8e9eeb49bfdfa5373ea610a7258c1d1aa34de686
James King <james.king@facepunchstudios.com>
2016-05-26 23:08:09 +0100

    Fixed setting entity angles

--- Assets/VBspViewer/Scripts/Importing/Entities/EntValue.cs
+++ Assets/VBspViewer/Scripts/Importing/Entities/EntValue.cs
@@ -252,7 +252,7 @@ namespace VBspViewer.Importing.Entities
 
         protected override Quaternion ToQuaternion()
         {
-            return Quaternion.Euler(new Vector3(Value.x, Value.z, Value.y));
+            return Quaternion.Euler(new Vector3(Value.x, 90f - Value.z, Value.y));
         }
     }
 
